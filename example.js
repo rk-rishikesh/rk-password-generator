b160f547d205d713cbf796a4ba03374c2bd0e3a3
Rishikesh Kale <59107121+rk-rishikesh@users.noreply.github.com>
2021-08-31 17:18:52 +0530

    Added test cases to example

--- example.js
+++ example.js
@@ -1,2 +1,4 @@
 const rkPasswordGenerator = require('rk-password-generator')
 console.log(rkPasswordGenerator("rishikeshKale"))
+console.log(rkPasswordGenerator("rvk.rishikesh"))
+console.log(rkPasswordGenerator("rvk.rishikeshkale@gmail.com"))
